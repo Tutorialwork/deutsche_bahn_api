be2ccb21adcc09db90a0c68c88ae8d3438100377
Tutorialwork <TutorialworkTV@gmail.com>
2023-01-15 15:43:26 +0100

    changed package name and website

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name="deutsche-bahn-api-Tutorialwork",
+    name="deutsche-bahn-api",
     version="1.0.0",
     author="Tutorialwork",
     author_email="mail@manuelschuler.dev",
     description="A small package to work with the Deutsche Bahn timetables api",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/clement-bonnet/medium-first-package",
+    url="https://github.com/Tutorialwork/deutsche_bahn_api",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
